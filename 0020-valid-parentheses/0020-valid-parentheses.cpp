587faffc1531e9b1cadf0c20dcc701a3f52c2c5e
Qianmo <qianmozhao294@gmail.com>
2026-01-25 20:04:04 +0800

    Time: 3 ms (9.47%), Space: 9 MB (38.04%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length()%2 != 0) return false;
+        stack <char> S1;
+        for(auto p = s.begin();p != s.end();p++){
+            if(*p == '{' || *p == '[' || *p == '('){
+                S1.push(*p);
+            }
+            else{
+                if(S1.empty()) return false;
+                char c = S1.top();
+                if(c == '(' && *p == ')'){
+                    S1.pop();
+                }
+                else if(c == '{' && *p == '}'){
+                    S1.pop();
+                }
+                else if(c == '[' && *p == ']'){
+                    S1.pop();
+                }
+                else return false;
+            }
+        }
+        return S1.empty();
+    }
+};
