1b714b58c6b8c3af9af9a68333518a070508490c
L0gicol <qianmozhao294@gmail.com>
2026-01-28 10:18:33 +0800

    新增 LeetCode 27 移除元素和 LeetCode 977 有序数组的平方的 C++ 实现

---
+++ My_Notes/Codes/0977_Squares_of_a_Sorted_Array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int> &nums)
+    {
+        int front = 0;
+        int tail = nums.size() - 1;
+        int n = nums.size();
+        int insert_index = n - 1;
+        vector<int> result(n);
+        while (front <= tail)
+        {
+            if (nums[front] * nums[front] >= nums[tail] * nums[tail])
+            {
+                result[insert_index] = nums[front] * nums[front];
+                front++;
+            }
+            else
+            {
+                result[insert_index] = nums[tail] * nums[tail];
+                tail--;
+            }
+            insert_index--;
+        }
+        return result;
+    }
+};
