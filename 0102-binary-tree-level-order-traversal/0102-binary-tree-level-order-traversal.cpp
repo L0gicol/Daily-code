c1a8aace21ff1798d3caacf33ad1fdc278dbff75
Qianmo <qianmozhao294@gmail.com>
2026-02-01 15:52:32 +0800

    Time: 0 ms (100%), Space: 17.1 MB (70.7%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,13 +12,11 @@ public:
             vector<int> temp;
             
             for(int i = 0; i < lenth; i++){
-                // 优化：用一个指针接住队头，方便后面操作
                 TreeNode* cur = que.front(); 
-                que.pop(); // 记得弹出！
+                que.pop();
                 
                 temp.push_back(cur->val);
                 
-                // ⚠️ 修正点：判空后再入队
                 if(cur->left != nullptr) que.push(cur->left);
                 if(cur->right != nullptr) que.push(cur->right);
             }
